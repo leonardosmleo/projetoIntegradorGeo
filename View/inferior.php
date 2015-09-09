c661b9b923307d7ca0124f225d852ba4604bdf8a
amandacortes <amandacortes.rodrigues@gmail.com>
2015-09-09 15:54:43 -0300

    .

--- View/inferior.php
+++ View/inferior.php
@@ -1,23 +1,3 @@
 </div>
-<<<<<<< HEAD
-=======
-        <!-- SCRIPTS -->
-        
-        <script src="../Libs/js/bootstrap.min.js"></script>
-        
-        <!-- Bootflat's JS files.-->
-        <script src="../Libs/bootstrapsourc/js/icheck.min.js"></script>
-        <script src="../Libs/bootflat/js/jquery.fs.selecter.min.js"></script>
-        <script src="../Libs/bootflat/js/jquery.fs.stepper.min.js"></script>
-
-        <!-- DataTables -->
-        <script type="text/javascript" charset="utf8" src="DataTables-1.10.7/media/js/jquery.dataTables.js"></script>
-        <script type="text/javascript" src="../Libs/js/bootstrap.min.js"></script>
-        <script type="text/javascript" url="//code.jquery.com/jquery-1.11.1.min.js"></script>
-        <script type="text/javascript" url="//cdn.datatables.net/1.10.7/js/jquery.dataTables.min.js"></script>
-        <script type="text/javascript" url="//cdn.datatables.net/plug-ins/1.10.7/integration/bootstrap/3/dataTables.bootstrap.js"></script>
-        <script type="text/javascript" src="../Libs/js/dist/jquery.validate.js"></script>
-
->>>>>>> 306702a3f1e5562337f2056369e6570a67b22d99
     </body>
 </html>
