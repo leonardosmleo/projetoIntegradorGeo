952be0afcd0c9a2fc035c0cb2dc7ef551c9b5426
Amanda Cortês Rodrigues <amandar.cortes@gmail.com>
2015-09-09 21:49:13 -0300

    .

--- View/inferior.php
+++ View/inferior.php
@@ -1,25 +1,3 @@
 </div>
-<<<<<<< HEAD
-        <!-- SCRIPTS -->
-        
-        <script src="../Libs/js/bootstrap.min.js"></script>
-
-        <script type="text/javascript" language= "javascript" src="js/jquery.min.js"></script>
-        
-        <!-- Bootflat's JS files.-->
-        <script src="../Libs/bootstrapsourc/js/icheck.min.js"></script>
-        <script src="../Libs/bootflat/js/jquery.fs.selecter.min.js"></script>
-        <script src="../Libs/bootflat/js/jquery.fs.stepper.min.js"></script>
-
-        <!-- DataTables -->
-        <script type="text/javascript" charset="utf8" src="DataTables-1.10.7/media/js/jquery.dataTables.js"></script>
-        <script type="text/javascript" src="../Libs/js/bootstrap.min.js"></script>
-        <script type="text/javascript" url="//code.jquery.com/jquery-1.11.1.min.js"></script>
-        <script type="text/javascript" url="//cdn.datatables.net/1.10.7/js/jquery.dataTables.min.js"></script>
-        <script type="text/javascript" url="//cdn.datatables.net/plug-ins/1.10.7/integration/bootstrap/3/dataTables.bootstrap.js"></script>
-        <script type="text/javascript" src="../Libs/js/dist/jquery.validate.js"></script>
-
-=======
->>>>>>> c661b9b923307d7ca0124f225d852ba4604bdf8a
     </body>
 </html>
